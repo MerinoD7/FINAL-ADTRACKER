9d661a6eed662fc29ddb5d6d7ba8ad16dfc1463c
Saul Merino <116820286+MerinoD7@users.noreply.github.com>
2023-10-23 11:24:21 -0600

    Add files via upload

---
+++ JS/FUnciones.js
@@ -0,0 +1,110 @@
+let ventanaPopup;
+
+function abrirPopup() {
+    const ventanaX = (window.innerWidth - 800) / 2; 
+    const ventanaY = (window.innerHeight - 800) / 2; 
+    
+    window.open("https://unimerelsalvador.typeform.com/to/DA5A2HWO", "MiVentanaEmergente", `width=800,height=900,left=${ventanaX},top=${ventanaY}`);
+
+    // Agregar un evento "load" al formulario dentro de la ventana emergente
+    ventanaPopup.addEventListener("load", function() {
+        const formulario = ventanaPopup.document.getElementById("MiVentanaEmergente");
+
+        formulario.addEventListener("submit-button", function(event) {
+            event.preventDefault(); // Evita que se envíe el formulario
+            // Aquí puedes realizar cualquier validación del formulario si es necesario
+
+            // Cerrar la ventana emergente
+            ventanaPopup.close();
+        });
+    });
+}
+
+    let ventanaPopup2;
+
+    function abrirPopup2() {
+        const ventanaX = (window.innerWidth - 800) / 2; 
+        const ventanaY = (window.innerHeight - 800) / 2; 
+        
+        window.open("https://checkout.baccredomatic.com/MmE2MGI3OTM1NDMzYzYuNDk5NTM0MjExNjk4MDc4OTIz", "MiVentanaEmergente", `width=800,height=1050,left=${ventanaX},top=${ventanaY}`);
+    
+        // Agregar un evento "load" al formulario dentro de la ventana emergente
+        ventanaPopup.addEventListener("load", function() {
+            const formulario = ventanaPopup2.document.getElementById("MiVentanaEmergente");
+    
+            formulario.addEventListener("submit-button", function(event) {
+                event.preventDefault(); // Evita que se envíe el formulario
+                // Aquí puedes realizar cualquier validación del formulario si es necesario
+    
+                // Cerrar la ventana emergente
+                ventanaPopup.close();
+            });
+        });
+    }
+
+
+    let ventanaPopup3;
+
+    function abrirPopup3() {
+        const ventanaX = (window.innerWidth - 800) / 2; 
+        const ventanaY = (window.innerHeight - 800) / 2; 
+        
+        window.open("https://checkout.baccredomatic.com/NjYzNjJhLjkwMDg0YTYzNjU1MDMzMTIxNjk4MDgwMjc4", "MiVentanaEmergente", `width=800,height=1000,left=${ventanaX},top=${ventanaY}`);
+    
+        // Agregar un evento "load" al formulario dentro de la ventana emergente
+        ventanaPopup.addEventListener("load", function() {
+            const formulario = ventanaPopup3.document.getElementById("MiVentanaEmergente");
+    
+            formulario.addEventListener("submit-button", function(event) {
+                event.preventDefault(); // Evita que se envíe el formulario
+                // Aquí puedes realizar cualquier validación del formulario si es necesario
+    
+                // Cerrar la ventana emergente
+                ventanaPopup.close();
+            });
+        });
+    }
+
+    let ventanaPopup4;
+
+    function abrirPopup4() {
+        const ventanaX = (window.innerWidth - 800) / 2; 
+        const ventanaY = (window.innerHeight - 800) / 2; 
+        
+        window.open("https://checkout.baccredomatic.com/MDRjNWE2MzQuMDhjMjI1NzY2NDVlNjgxNjk4MDgwMzQz", "MiVentanaEmergente", `width=800,height=1000,left=${ventanaX},top=${ventanaY}`);
+    
+        // Agregar un evento "load" al formulario dentro de la ventana emergente
+        ventanaPopup.addEventListener("load", function() {
+            const formulario = ventanaPopup4.document.getElementById("MiVentanaEmergente");
+    
+            formulario.addEventListener("submit-button", function(event) {
+                event.preventDefault(); // Evita que se envíe el formulario
+                // Aquí puedes realizar cualquier validación del formulario si es necesario
+    
+                // Cerrar la ventana emergente
+                ventanaPopup.close();
+            });
+        });
+    }
+
+    let ventanaPopup5;
+
+    function abrirPopup5() {
+        const ventanaX = (window.innerWidth - 800) / 2; 
+        const ventanaY = (window.innerHeight - 800) / 2; 
+        
+        window.open("https://checkout.baccredomatic.com/NDJhNjM4MDMzNTdlNTk2YTE0MTYuNzAxNjk4MDgwNDE5", "MiVentanaEmergente", `width=800,height=1000,left=${ventanaX},top=${ventanaY}`);
+    
+        // Agregar un evento "load" al formulario dentro de la ventana emergente
+        ventanaPopup.addEventListener("load", function() {
+            const formulario = ventanaPopup5.document.getElementById("MiVentanaEmergente");
+    
+            formulario.addEventListener("submit-button", function(event) {
+                event.preventDefault(); // Evita que se envíe el formulario
+                // Aquí puedes realizar cualquier validación del formulario si es necesario
+    
+                // Cerrar la ventana emergente
+                ventanaPopup.close();
+            });
+        });
+    }
